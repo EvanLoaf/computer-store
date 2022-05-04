d4f4556a1d2a0a5aedcce99dbf27d1d265b28cec
Ivan Karavai <evanloafakahaitao@gmail.com>
2022-05-04 11:45:10 +0300

    Switch hibernate versions

---
+++ dao/src/main/java/com/gmail/evanloafakahaitao/computer/store/config/properties/HibernateProperties.java
@@ -0,0 +1,4 @@
+package com.gmail.evanloafakahaitao.computer.store.config.properties;
+
+public interface HibernateProperties {
+}
---
+++ dao/src/main/java/com/gmail/evanloafakahaitao/computer/store/dao/util/HibernateUtil.java
@@ -0,0 +1,4 @@
+package com.gmail.evanloafakahaitao.computer.store.dao.util;
+
+public class HibernateUtil {
+}
