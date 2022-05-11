24addbbeb30f0a653cef128a47f3739ae2b88247
Ivan Karavai <evanloafakahaitao@gmail.com>
2022-05-11 21:51:36 +0300

    Add jackson LocalDateTime serialization support

---
+++ web/src/main/java/com/gmail/evanloafakahaitao/computer/store/controllers/api/BusinessCardAPIController.java
@@ -0,0 +1,4 @@
+package com.gmail.evanloafakahaitao.computer.store.controllers.api;
+
+public class BusinessCardAPIController {
+}
---
+++ web/src/main/java/com/gmail/evanloafakahaitao/computer/store/controllers/model/APIResponse.java
@@ -0,0 +1,4 @@
+package com.gmail.evanloafakahaitao.computer.store.controllers.model;
+
+public class APIResponse {
+}
